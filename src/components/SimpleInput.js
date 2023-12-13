f37df6855d51e2173b870743a3bcb1c75766ab3b
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-13 12:23:56 +0530

    04th-video dealing with form submission getting user input with useRef and useSate

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,48 @@
+
+import { useRef, useState } from 'react';
 const SimpleInput = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const enteredInputRef =  useRef();
+
+  //  ! Two approaches to handle the status of the input
+  // ! 1. Use the useState
+  // ! 2. Use the useRef
+
+  // * useState
+  // ! When we need to track the state of input for every keystroke
+  // ! If we want value on every keystroke, we can use useState for instance validation. 
+  // ! If want to reset the value, we can use useState
+
+  // * useRef
+  // ! we read the value when needed it using the useRef
+  // ! If we want the value at once, we can use the useRef
+  
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = (event) => {
+    // ! The javascript default behavior is when submitting a form, it sends a request to the server. so it will refresh the page.
+    // ! .... To prevent that default behavior, we use event.preventDefault();
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(enteredInputRef.current.value);
+
+    // ! Don`t do this with refs!
+    // ! enteredInputRef.current.value = ''; // NOT IDEAL, DON'T MANIPULATE THE DOM
+    // ! instead, use state to achieve the same result
+
+    setEnteredName('');
+
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={enteredInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
